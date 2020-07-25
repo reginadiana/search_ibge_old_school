c88de89f71448d14ab3af8666d98091c2759415f
reginadiana <dianaregina22@outlook.com.br>
2020-07-25 19:26:09 -0300

    Usuário é capaz de acessar o menu com as opções e digitar os dados

---
+++ lib/menu.rb
@@ -0,0 +1,85 @@
+require 'colorize'
+
+# Options
+EXIT = 0
+COMMON_NAMES_IN_UF = 1
+COMMON_NAMES_IN_COUNTY = 2
+FREQUETY_NAMES = 3
+MOST_USED_DECADE = 4
+
+class Menu
+  def welcome
+    puts 'Bem-vindo a plataforma de pesquisa de nomes IBGE!'.green
+  end
+
+  def show
+    print "\nMenu:\n".yellow
+    puts "[#{COMMON_NAMES_IN_UF}] Ver nomes mais comuns de uma Unidade Federativa (UF)".yellow
+    puts "[#{COMMON_NAMES_IN_COUNTY}] Ver nomes mais comuns de um municipio".yellow
+    puts "[#{FREQUETY_NAMES}] Ver frequência de um nome ao longo dos anos".yellow
+    puts "[#{MOST_USED_DECADE}] Ver nomes mais usados em uma década".yellow
+    puts "[#{EXIT}] Sair".yellow
+
+    print 'Escolha uma opção: '
+    read_number
+  end
+
+  def response_consult(option)
+    if option == FREQUETY_NAMES
+      insert_names
+      
+    end
+  end
+
+  def insert_uf
+    print 'Digite o código da UF:'
+    read_input
+  end
+
+  def insert_county
+    print 'Digite o código do Municipio:'
+    read_input
+  end
+
+  def insert_names
+    print "Digite um nome ou vários separados por vírgulas, exemplo:\n"
+    print "Para um nome, digite: Diana\n"
+    print "Para vários nomes, digite: Diana, João, Camilo, Luiz\n"
+    separate_names(read_input)
+  end
+
+  def separate_names(input)
+    names = input.split(',')
+    names.each do |name|
+      puts name.split(' ')
+    end
+  end
+
+  def search_not_found
+    puts 'Não foi possível encontrar os dados, tente novamente'.red
+  end
+
+  def check_invalidation_option(option)
+    if option < 0 or option > 4
+      puts "\nPor favor, digite uma opção válida\n".red
+      true
+    end
+  end
+
+  def decorate
+    puts '-' * 60
+  end
+
+  def clear
+    system('clear')
+    puts
+  end
+
+  def read_input
+    gets.chomp
+  end
+
+  def read_number
+    gets.to_i
+  end
+end
--- lib/search.rb
+++ lib/search.rb
@@ -1,66 +1,21 @@
 # frozen_string_literal: true
 
 require 'io/console'
-require 'sqlite3'
 require 'colorize'
-require_relative 'sql'
+require_relative 'menu'
 
-db = SQLite3::Database.open 'db/database.db'
+menu = Menu.new
+menu.welcome
+option = menu.show
 
-def menu
-
-  print "\nUnidades Federativas (UF):\n"
-  read_data('federatives')
-
-  print "\nMenu:\n".yellow
-
-  puts "[#{SAIR}] Sair".yellow
-
-  print 'Escolha uma opção: '
-  gets.to_i
+while option != EXIT
+  menu.clear
+  if not menu.check_invalidation_option(option)
+    menu.response_consult(option)
+  end
+  option = menu.show
 end
 
-def decorate
-  puts '-' * 60
-end
-
-def welcome
-  puts 'Bem-vindo a plataforma de pesquisa do IBGE!'.green
-end
-
-def clear
-  system('clear')
-  puts
-end
-
-def read_input
-  gets.chomp
-end
-
-def insert_uf
-  print 'Digite o código da UF:'
-  read_input
-end
-
-def insert_county
-  print 'Digite o código do Municipio:'
-  read_input
-end
-
-def continue
-  puts "\nEscolha uma opção:"
-  gets.to_i
-end
-
-def search_not_found
-  puts 'Não foi possível encontrar os dados, tente novamente'.red
-end
-
-clear
-welcome
-opcao = menu
-
-
-if opcao == SAIR
+if option == EXIT
   puts "\nObrigada por acessar a nossa plataforma de pesquisa :)\n".green
 end
